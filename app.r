f547c6eed1388ea6f72673c7654a6e9c2e4c113b
Ilvondir <komsa.m@o2.pl>
2023-02-12 18:27:04 +0100

    login function added

--- app.r
+++ app.r
@@ -11,17 +11,71 @@ books <- readRDS("datasets/books.rds")
 ui <- uiOutput("page")
 
 # Server ----
-server <- function(input, output) {
-  ## Login Panel layout ----
+server <- function(input, output, session) {
+  ## Pages ----
+  
+  ### Login panel ----
   loginPanel <- fluidPage( useShinyjs(), includeHTML("www/loginPanel.html") )
+  
+  ### Navbar page ----
+  
+  
+  ### UI setting ----
   output$page <- renderUI(loginPanel)
   
-  ## Login function ----
+  
+  ## Functions ----
+  ### Login function ----
   observeEvent(input$loginButton, {
     login <- input$login
     password <- input$password
     
-    
+    if (login=="" | password=="") {
+      updateTextInput(session, "login", value="")
+      updateTextInput(session, "password", value="")
+      html("result", "Enter all information")
+    } else {
+      
+      id <- accounts %>%
+        filter(login==Login) %>%
+        select(ID)
+      
+      id <- ifelse(nrow(id)==0, -1, id$ID[1])
+      
+      if (id==-1) {
+        updateTextInput(session, "login", value="")
+        updateTextInput(session, "password", value="")
+        html("result", "User not found")
+      } else {
+        
+        pass <- accounts %>%
+          filter(ID==id) %>%
+          select(Password)
+        
+        pass <- pass$Password[1]
+        
+        if (password!=pass) {
+          updateTextInput(session, "login", value="")
+          updateTextInput(session, "password", value="")
+          html("result", "Wrong password entered")
+        } else {
+          
+          role <- accounts %>%
+            filter(ID==id) %>%
+            select(Role)
+          role <- role$Role[1]
+          
+          updateTextInput(session, "login", value="")
+          updateTextInput(session, "password", value="")
+          html("result", "")
+          
+          showModal(modalDialog(
+            title = "Login was successful!",
+            paste0("Hello!\nYou have logged in as ", role, ".")
+          ))
+        }
+      }
+    }
   })
 }
 
