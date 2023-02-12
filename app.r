5f2ca51e05ea09cece2024c65ade913a1e840d64
Ilvondir <komsa.m@o2.pl>
2023-02-12 17:37:29 +0100

    html with R associated

--- app.r
+++ app.r
@@ -1,16 +1,28 @@
 # Libraries ----
 library(shiny)
+library(tidyverse)
+library(shinyjs)
+
+# Data initialization ----
+accounts <- readRDS("datasets/accounts.rds")
+books <- readRDS("datasets/books.rds")
 
 # UI ----
 ui <- uiOutput("page")
 
 # Server ----
-server <- function(input, output, session) {
+server <- function(input, output) {
   ## Login Panel layout ----
-  loginPanel <- fluidPage( includeHTML("www/loginPanel.html") )
-  
-  
+  loginPanel <- fluidPage( useShinyjs(), includeHTML("www/loginPanel.html") )
   output$page <- renderUI(loginPanel)
+  
+  ## Login function ----
+  observeEvent(input$loginButton, {
+    login <- input$login
+    password <- input$password
+    
+    
+  })
 }
 
 # Shiny App ----
